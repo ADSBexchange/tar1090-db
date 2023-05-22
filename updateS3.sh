ac28d66e4da821046e3421974672e7230da4ab29
Bryan Landerman <bryan@silversmith.com>
2023-05-22 14:07:15 -0400

    Updates to fix git commit/push and S3 cp

--- updateS3.sh
+++ updateS3.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [[ -z ${BUCKET_NAME} ]]; then
-    echo "The bucket name of the destination for database updates is required as an environment variable."
+if [[ -z ${BUCKET_NAME} || -z ${GIT_EMAIL} || -z ${GIT_NAME} ]]; then
+    echo "The destination bucket name (BUCKET_NAME), git email (GIT_EMAIL), and git user name (GIT_NAME) are required as environment variables."
 else
   REPO="tar1090-db"
+  git config user.email "${GIT_EMAIL}"
+  git config user.name "${GIT_NAME}"
 
   # archive old tar1090-db
   aws s3 mv "s3://${BUCKET_NAME}/${REPO}/latest" "s3://${BUCKET_NAME}/${REPO}/$(date +%F-%H-%M-%S-%N)"
@@ -14,7 +16,7 @@ else
   ./update.sh
 
   # push updated db folder to S3
-  aws s3 cp db "s3://${BUCKET_NAME}/${REPO}/latest/db"
+  aws s3 cp db "s3://${BUCKET_NAME}/${REPO}/latest/db" --recursive
 
   # generate updated csv on csv branch of tar1090-db
   ./csv.sh
